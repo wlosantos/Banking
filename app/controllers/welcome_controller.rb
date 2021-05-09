19b592bcc2b8a31f36354a53aa4442a79e030310
Wendel Lopes <wendelopes@msn.com>
2021-05-09 17:52:10 -0300

    Adding period to the extract

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,4 +16,29 @@ class WelcomeController < ApplicationController
     @order = Order.new
   end
 
+  def extrato
+  look = params['date_start'].nil?
+
+  if look
+    @inicio = Date.today - 5
+    @fim = Date.today + 1
+  elsif params['date_start'] == '' || params['date_end'] == ''
+    @inicio = Date.today - 5
+    @fim = Date.today + 1
+  elsif params['date_start'] > params['date_end']
+    @inicio = Date.today - 5
+    @fim = Date.today + 1
+  else
+    @inicio = params['date_start'].to_datetime
+    @fim = change_date(params['date_end']).to_datetime
+  end
+end
+
+private
+
+def change_date(data)
+  dt = data.split('-')
+  set_data = Date.new(dt[0].to_i,dt[1].to_i,dt[2].to_i + 1)
+end
+
 end
