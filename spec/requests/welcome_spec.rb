1d272ed3a8943b64330e8e6ff29924242e06e695
Wendel Lopes <wendelopes@msn.com>
2021-05-09 16:04:44 -0300

    Adding deposito page with test

--- spec/requests/welcome_spec.rb
+++ spec/requests/welcome_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "Welcomes", type: :request do
 
   describe 'GET /index' do
+    let!(:account) { create(:account) }
     before do
-      sign_in create(:account)
+      sign_in account
       get root_path
     end
 
@@ -18,6 +19,25 @@ RSpec.describe "Welcomes", type: :request do
 
     it 'return the content' do
       expect(response.body).to include('nobe-Bank')
+    end
+  end
+
+  describe 'GET /deposito' do
+    before do
+      sign_in create(:account)
+      get deposito_path
+    end
+
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'returns status code 200' do
+      expect(response.status).to eql(200)
+    end
+
+    it 'return the content' do
+      expect(response.body).to include('Depósito')
     end
   end
 
